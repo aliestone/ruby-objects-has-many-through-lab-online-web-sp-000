cfb1b4bbebbafe686cc46a39dfc0addfaa8b48d4
Alie Stone <helloaliestone@gmail.com>
2020-10-08 14:47:08 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,12 +17,13 @@ class Patient
   end
 
   def appointments
-    Appointment.all.detect do |appointment|
+    Appointment.all.select do |appointment|
     appointment.patient == self
   end
 
   def doctors
     appointments.collect do |appointment|
+    appointment.doctor
   end
 end
 end
