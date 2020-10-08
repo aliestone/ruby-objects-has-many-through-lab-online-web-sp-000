1502fd1f69077d57a4ee973b5c6840c79e76788f
Alie Stone <helloaliestone@gmail.com>
2020-10-08 13:56:04 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,8 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
+    Appointment.all.select {|appointment|
+    do appointment.doctor == self}
   end
 
   def patients
