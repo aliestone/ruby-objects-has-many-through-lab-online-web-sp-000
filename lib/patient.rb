2b7ca4a110d0c796f06e951a270a307cd8c1ddd7
Alie Stone <helloaliestone@gmail.com>
2020-10-08 14:38:28 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,18 +11,21 @@ class Patient
   def self.all
     @@all
   end
-
+  
   def new_appointment(date, doctor)
-    patient(date, self, doctor)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
-    Appointment.all{|appointment| appointment.patient == self}
+    Appointment.all.select do |appointment|
+    appointment.patient == self
+     end
+   end
+
+   def doctors
+    appointments.collect do |appointment|
+      end
+    end
+
   end
-
-  def patient
-    self
-  end
-
-
 end
